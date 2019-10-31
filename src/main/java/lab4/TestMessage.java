d19f72f88a907727b929a2b748ff6b05a48c82b8
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 17:27:11 +0300

    Scripted auto-commit on change (2019-10-31 17:27:11) by gitwatch.sh

--- src/main/java/lab4/TestMessage.java
+++ src/main/java/lab4/TestMessage.java
@@ -4,5 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
 @JsonAutoDetect
 public class TestMessage {
-    
+
+    private int packageId;
+    private String jsScript;
+    private String functionName;
+    private 
 }
