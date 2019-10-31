1597946a0007372e4cd2b550e4e65e7a27b07496
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 15:51:28 +0300

    Scripted auto-commit on change (2019-10-31 15:51:28) by gitwatch.sh

--- src/main/java/lab4/TestPerformerActor.java
+++ src/main/java/lab4/TestPerformerActor.java
@@ -1,4 +1,10 @@
 package lab4;
 
-public class TestPerformerActor {
+import akka.actor.AbstractActor;
+
+public class TestPerformerActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
