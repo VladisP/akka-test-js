28393c82cbee7db0ac13e33131abb9e267e58e7e
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 15:55:21 +0300

    Scripted auto-commit on change (2019-10-31 15:55:21) by gitwatch.sh

--- src/main/java/lab4/Launcher.java
+++ src/main/java/lab4/Launcher.java
@@ -10,6 +10,6 @@ public class Launcher {
 
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
-        ActorRef rootActor = system.actorOf(Props.create())
+        ActorRef rootActor = system.actorOf(Props.create(RootActor.class));
     }
 }
