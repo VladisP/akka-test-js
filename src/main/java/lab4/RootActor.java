89080dd4fe060c004870a6d7470c3f61ce3b9f55
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 15:52:13 +0300

    Scripted auto-commit on change (2019-10-31 15:52:13) by gitwatch.sh

--- src/main/java/lab4/RootActor.java
+++ src/main/java/lab4/RootActor.java
@@ -7,7 +7,7 @@ import akka.actor.Props;
 public class RootActor extends AbstractActor {
 
     private ActorRef storeActor = getContext().actorOf(Props.create(StoreActor.class));
-    private ActorRef testRouter = 
+    private ActorRef testRouter = getContext().actorOf()
 
     @Override
     public Receive createReceive() {
