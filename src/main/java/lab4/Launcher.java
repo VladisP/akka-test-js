82ecf397e6a87cad462f6b1463a4e5dc5a49ae4c
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 16:12:07 +0300

    Scripted auto-commit on change (2019-10-31 16:12:07) by gitwatch.sh

--- src/main/java/lab4/Launcher.java
+++ src/main/java/lab4/Launcher.java
@@ -4,6 +4,7 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
@@ -18,6 +19,8 @@ import java.util.concurrent.CompletionStage;
 public class Launcher extends AllDirectives {
 
     private static final String ACTOR_SYSTEM_NAME = "test-js";
+    private static final String HOST_NAME = "localhost";
+    private static final int PORT = 8080;
 
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
@@ -30,8 +33,15 @@ public class Launcher extends AllDirectives {
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 instance.createRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-
+                routeFlow
+                , ConnectHttp.toHost(HOST_NAME, PORT)
+                , materializer
         );
+
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 
     private Route createRoute() {
