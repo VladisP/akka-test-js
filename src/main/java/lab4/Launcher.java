6a1f389912628bc4240acc24d2b300587c8722e2
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 16:04:54 +0300

    Scripted auto-commit on change (2019-10-31 16:04:54) by gitwatch.sh

--- src/main/java/lab4/Launcher.java
+++ src/main/java/lab4/Launcher.java
@@ -1,11 +1,15 @@
 package lab4;
 
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 public class Launcher extends AllDirectives {
 
@@ -17,6 +21,10 @@ public class Launcher extends AllDirectives {
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        
+        Launcher instance = new Launcher();
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =;
     }
+
+    crero
 }
