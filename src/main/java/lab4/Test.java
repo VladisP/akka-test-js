d745f7ff7877299079f4f15e3b38585e8772e3fd
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 17:35:50 +0300

    Scripted auto-commit on change (2019-10-31 17:35:50) by gitwatch.sh

--- src/main/java/lab4/Test.java
+++ src/main/java/lab4/Test.java
@@ -9,5 +9,30 @@ public class Test {
     private String expectedResult;
     private int[] params;
 
-    
+    public Test() {
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public void setExpectedResult(String expectedResult) {
+        this.expectedResult = expectedResult;
+    }
+
+    public int[] getParams() {
+        return params;
+    }
+
+    public void setParams(int[] params) {
+        this.params = params;
+    }
 }
--- src/main/java/lab4/TestMessage.java
+++ src/main/java/lab4/TestMessage.java
@@ -8,5 +8,40 @@ public class TestMessage {
     private int packageId;
     private String jsScript;
     private String functionName;
-    private
+    private Test[] tests;
+
+    public TestMessage() {
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public void setPackageId(int packageId) {
+        this.packageId = packageId;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public void setJsScript(String jsScript) {
+        this.jsScript = jsScript;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public void setFunctionName(String functionName) {
+        this.functionName = functionName;
+    }
+
+    public Test[] getTests() {
+        return tests;
+    }
+
+    public void setTests(Test[] tests) {
+        this.tests = tests;
+    }
 }
