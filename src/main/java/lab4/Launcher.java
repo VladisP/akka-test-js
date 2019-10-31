13a83652bfe21c4bdefbdc4cc7201b5e95270f7f
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 14:23:29 +0300

    Scripted auto-commit on change (2019-10-31 14:23:29) by gitwatch.sh

--- src/main/java/lab4/Launcher.java
+++ src/main/java/lab4/Launcher.java
@@ -4,7 +4,9 @@ import akka.actor.ActorSystem;
 
 public class Launcher {
 
+    private static final String ACTOR_SYSTEM_NAME = ""
+
     public static void main(String[] args) {
-        ActorSystem system = ActorSystem.create()
+        ActorSystem system = ActorSystem.create();
     }
 }
