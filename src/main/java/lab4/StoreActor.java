a815414928f2550852554e05009683ce4a8a732e
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 20:08:49 +0300

    Scripted auto-commit on change (2019-10-31 20:08:49) by gitwatch.sh

--- src/main/java/lab4/StoreActor.java
+++ src/main/java/lab4/StoreActor.java
@@ -3,32 +3,15 @@ package lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-
 public class StoreActor extends AbstractActor {
+
+    private 
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(TestMessage.class, msg -> {
-                    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-                    engine.eval(msg.getJsScript());
-                    Invocable invocable = (Invocable) engine;
-                    Test test = msg.getTests()[0];
-                    Object[] params = test.getParams();
-                    String result = invocable.invokeFunction(msg.getFunctionName(), params).toString();
+                .match(TestResultMessage.class, msg -> {
 
-                    getContext().actorSelection("/user/rootActor/storeActor")
-                            .tell(new TestResultMessage(
-                                            msg.getPackageId(),
-                                            result.equals(test.getExpectedResult()),
-                                            result,
-                                            test
-                                    ),
-                                    self()
-                            );
                 })
                 .build();
     }
