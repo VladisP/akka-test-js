4940ce5783e4c87d8062adecf71446786c62ad70
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 19:55:57 +0300

    Scripted auto-commit on change (2019-10-31 19:55:57) by gitwatch.sh

--- src/main/java/lab4/TestResultMessage.java
+++ src/main/java/lab4/TestResultMessage.java
@@ -2,5 +2,31 @@ package lab4;
 
 public class TestResultMessage {
 
+    private int packageId;
+    private boolean isSuccessful;
+    private String result;
+    private Test test;
 
+    public TestResultMessage(int packageId, boolean isSuccessful, String result, Test test) {
+        this.packageId = packageId;
+        this.isSuccessful = isSuccessful;
+        this.result = result;
+        this.test = test;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public Test getTest() {
+        return test;
+    }
 }
