3120f1f9e33329d56ce5fdd011ae49032f2f61da
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 15:47:40 +0300

    Scripted auto-commit on change (2019-10-31 15:47:40) by gitwatch.sh

--- src/main/java/lab4/StoreActor.java
+++ src/main/java/lab4/StoreActor.java
@@ -1,4 +1,10 @@
 package lab4;
 
-public class StoreActor {
+import akka.actor.AbstractActor;
+
+public class StoreActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
