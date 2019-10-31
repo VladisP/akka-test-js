6a4072f5fc7cbd37a2b76711489d399bf3e9c102
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 19:36:28 +0300

    Scripted auto-commit on change (2019-10-31 19:36:28) by gitwatch.sh

--- src/main/java/lab4/TestPerformerActor.java
+++ src/main/java/lab4/TestPerformerActor.java
@@ -12,7 +12,7 @@ public class TestPerformerActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(TestMessage.class, msg -> {
-                    ScriptEngine engine = new ScriptEngineManager().getEngineByName()
+                    ScriptEngine engine = new ScriptEngineManager().getEngineByName("nash")
 
                     for (Test test : msg.getTests()) {
                         testRouter.tell(new TestMessage(
