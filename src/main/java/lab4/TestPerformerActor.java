a3117c038befae0082600ab8bc5b5a7cbd6ed443
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-31 19:38:18 +0300

    Scripted auto-commit on change (2019-10-31 19:38:18) by gitwatch.sh

--- src/main/java/lab4/TestPerformerActor.java
+++ src/main/java/lab4/TestPerformerActor.java
@@ -16,7 +16,7 @@ public class TestPerformerActor extends AbstractActor {
                     ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
                     engine.eval(msg.getJsScript());
                     Invocable invocable = (Invocable) engine;
-                    String result invocable.invokeFunction()
+                    String result invocable.invokeFunction(msg.getFunctionName(), )
 
                     for (Test test : msg.getTests()) {
                         testRouter.tell(new TestMessage(
